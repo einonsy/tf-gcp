9c312ee504c0faadddbd0a14fe85d88be4be0927
Jason Einon <jasoneinon@Jasons-iMac.broadband>
2020-07-18 12:19:57 +0100

    new: firewall rule to allow access on port 5000

--- main.tf
+++ main.tf
@@ -26,3 +26,13 @@ resource "google_compute_instance" "default" {
    }
  }
 }
+
+resource "google_compute_firewall" "default" {
+ name    = "flask-app-firewall"
+ network = "default"
+
+ allow {
+   protocol = "tcp"
+   ports    = ["5000"]
+ }
+}
